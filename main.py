302d5daca9fafca352bc36b74aa32f3c7c0c6b31
Nicholas Zivkovic <znichola@student.42nice.fr>
2026-01-14 09:23:56 +0100

    reset main to before break

--- main.py
+++ main.py
@@ -8,21 +8,20 @@ import utils as u
 C = 1
 
 class state:
-    def __init__(self, grid: tuple, h):
+    def __init__(self, grid, h):
         self.predecessor: Optional[int] = None
-        self.g = 0
-        self.f = 0
+        self.cost = 0
         self.h = h
         self.grid = grid
 
     def __repr__(self) -> str:
         return u.gridToString(self.grid)
-    
-    def __lt__(self, other):
-        return self.f > other.f
-    
-    def updateFScore(self):
-        self.f = self.g + self.h
+
+    def g(self):
+        return self.cost
+
+    def id(self):
+        return str(self.grid)
 
 
 class board:
@@ -166,64 +165,69 @@ class board:
         return len(tab)
 
 
-    def algo(self, grid: tuple):
-        opened = [grid] ; heapq.heapify(opened)
-        opened_set = set([grid])
+    def algo(self, grid):
+        opened =[(0, str(grid))]
+        opened_set = set([str(grid)])
         self.totoalStatesOpened = 1
         closed = set()
 
         succes = False
         while len(opened) != 0 and succes is False:
-            e_hash = heapq.heappop(opened); opened_set.remove(e_hash) # Select state by minimum heuristic
+            h, e_hash = heapq.heappop(opened) # Select state by minimum heuristic
+            # print(h, e_hash)
+            # print(h)
             e_state = self.states[e_hash]
             if e_state.grid == self.target:
-                self.last_state_id = e_state.grid
+                self.last_state_id = str(e_state.grid)
                 succes = True
             else:
+                opened_set.remove(e_hash)
                 closed.add(e_hash)
-                ee_hash = self.expand(e_state.grid)
+                ee_hash = self.expand(e_hash)
                 for s_hash in ee_hash:
                     s_state = self.states[s_hash]
                     if (not s_hash in opened_set) and (not s_hash in closed):
-                        opened_set.add(s_hash); heapq.heappush(opened, (s_state.h, s_hash)); self.totoalStatesOpened += 1
+                        heapq.heappush(opened, (s_state.h, s_hash)); self.totoalStatesOpened += 1
+                        opened_set.add(s_hash)
                         s_state.predecessor = e_hash
-                        s_state.g = e_state.g + C
+                        s_state.cost = e_state.cost + C
                     else:
-                        if s_state.g + s_state.h > e_state.g + C + s_state.h:
+                        if s_state.cost + s_state.h > e_state.cost + C + s_state.h:
                             s_state.predecessor = e_hash
-                            s_state.g = e_state.g + C
+                            s_state.cost = e_state.cost + C
                             if s_hash in closed:
                                 closed.remove(s_hash)
-                                opened_set.add(s_hash); heapq.heappush(opened, (s_state.h, s_hash)); self.totoalStatesOpened += 1
+                                heapq.heappush(opened, (s_state.h, s_hash)); self.totoalStatesOpened += 1
+                                opened_set.add(s_hash)
 
-    # def algo2(self, grid):
-    #     opened =[(0, str(grid))]
-    #     opened_set = set([str(grid)])
-    #     self.totoalStatesOpened = 1
-    #     closed = set()
-    #     succes = False
-    #     while len(opened) != 0 and succes is False:
-    #         h, e_hash = heapq.heappop(opened) # Select state by minimum heuristic
-    #         # print(h, e_hash)
-    #         # print(h)
-    #         if self.states[e_hash].grid == self.target:
-    #             self.last_state_id = str(self.states[e_hash].grid)
-    #             succes = True
-    #         if e_hash in closed:
-    #             continue
-    #         ee_hash = self.expand(e_hash)
-    #         for s_hash in ee_hash:
-    #             heapq.heappush(opened, (self.states[s_hash].cost + self.states[s_hash].h, s_hash)); self.totoalStatesOpened += 1
-    #         closed.add(e_hash)
+    def algo2(self, grid):
+        opened =[(0, str(grid))]
+        opened_set = set([str(grid)])
+        self.totoalStatesOpened = 1
+        closed = set()
+        succes = False
+        while len(opened) != 0 and succes is False:
+            h, e_hash = heapq.heappop(opened) # Select state by minimum heuristic
+            # print(h, e_hash)
+            # print(h)
+            if self.states[e_hash].grid == self.target:
+                self.last_state_id = str(self.states[e_hash].grid)
+                succes = True
+            if e_hash in closed:
+                continue
+            ee_hash = self.expand(e_hash)
+            for s_hash in ee_hash:
+                heapq.heappush(opened, (self.states[s_hash].cost + self.states[s_hash].h, s_hash)); self.totoalStatesOpened += 1
+            closed.add(e_hash)
 
 
-    def createState(self, grid: tuple):
-        self.states[grid] = state(grid, self.heuristic(grid))
+    def createState(self, grid):
+        self.states[str(grid)] = state(grid, self.heuristic(grid))
         self.current_id += 1
 
 
-    def findOrCreateState(self, grid: tuple) -> state:
-        g_hash = grid
+    def findOrCreateState(self, grid) -> state:
+        g_hash = str(grid)
         s = self.states.get(g_hash)
         if s == None:
             self.createState(grid)
@@ -231,7 +235,7 @@ class board:
         return s
 
 
-    def expand(self, state: tuple) -> list[tuple]:
+    def expand(self, state_hash: str) -> list[str]:
         '''returns a list of ids'''
         def getNeighbouringStates(state_hash):
             def getNeighbours(index):
@@ -245,12 +249,12 @@ class board:
             index = current_grid.index(0)
             neighbours = getNeighbours(index)
             for n in neighbours:
-                newState = list(current_grid)
+                newState = current_grid.copy()
                 newState[n], newState[index] = newState[index], newState[n]
-                states.append(tuple(newState))
+                states.append(newState)
             return states
-        grids = getNeighbouringStates(state)
-        return [self.findOrCreateState(g).grid for g in grids]
+        grids = getNeighbouringStates(state_hash)
+        return [str(self.findOrCreateState(g).grid) for g in grids]
 
 
 
@@ -268,13 +272,13 @@ def main():
             puzzle_data = f.read()
             size, grid = u.puzzleParser(puzzle_data)
 
-    b = board(size, tuple(grid), args.heuristic)
+    b = board(size, grid, args.heuristic)
     
     if b.isSolvable:
         if args.alt_algo:
             b.algo2(grid)
         else:
-            b.algo(tuple(grid))
+            b.algo(grid)
         print(f"h = {b.h}")
         print("Total number of states ever selected in the opened set : ", b.totoalStatesOpened)
         print("Maximum number of states ever represented in memory at the same time during the search : ", len(b.states))
